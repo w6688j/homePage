7a109d31c9d484ca697172335795e12fd10a0aab
root <953372680@qq.com>
2018-06-09 10:32:32 +0800

    finished

--- public/js/homepage/app/app.js
+++ public/js/homepage/app/app.js
@@ -1,6 +1,6 @@
 define(['angular', 'angular-async-loader', 'module', 'exports', 'functions'],
     function (angular, asyncLoader, module, exports) {
-        let app = angular.module('laravel', ['ui.router', 'ajaxLoading']);
+        var app = angular.module('laravel', ['ui.router', 'ajaxLoading']);
         asyncLoader.configure(app);
         module.exports = app;
 
